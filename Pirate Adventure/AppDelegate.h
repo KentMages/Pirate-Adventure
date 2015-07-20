e8b7975bd08a9167107abb016e6939039f5ffdc5
Kent H. Mages <kent.mages@gmail.com>
2015-07-20 10:45:39 -0500

    Initial Commit

---
+++ Pirate Adventure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Kent H. Mages on 7/20/15.
+//  Copyright (c) 2015 Kent H. Mages. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pirate Adventure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adventure
+//
+//  Created by Kent H. Mages on 7/20/15.
+//  Copyright (c) 2015 Kent H. Mages. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
